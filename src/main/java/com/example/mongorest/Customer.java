e59b1d73c07ce89c4b7cf4fe56273c8a5b141d85
ch <mkmsacc@outlook.com>
2022-01-30 19:02:34 +0700

    Remove @Data

--- src/main/java/com/example/mongorest/Customer.java
+++ src/main/java/com/example/mongorest/Customer.java
@@ -1,25 +1,22 @@
 package com.example.mongorest;
 
-import org.springframework.data.annotation.Id;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @ToString
 public class Customer {
-	
-	@Id
+
+    @Id
 //	@ToString.Exclude
-	private String id;
-	
-	private String firstName;
-	private String lastName;
+    private String id;
+
+    private String firstName;
+    private String lastName;
 
 }
